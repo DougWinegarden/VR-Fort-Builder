a3c002103e125e54c4c1115848b2a3fc4ee133ec
Doug Winegarden <d3wine@gmail.com>
2017-12-02 18:24:51 -0800

    fixing assets

---
+++ Assets/ASL/Resources/FortBuilder/camDropDownBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class camDropDownBehavior : MonoBehaviour {
+
+    public Dropdown camDropdown;
+
+    private int lastVal = 0;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lastVal != camDropdown.value)
+        {
+            lastVal = camDropdown.value;
+
+
+            if (camDropdown.value == 0)
+            {
+                Debug.Log("CamPos1");
+
+            }
+            if (camDropdown.value == 1)
+            {
+                Debug.Log("CamPos2");
+
+            }
+            if (camDropdown.value == 2)
+            {
+                Debug.Log("CamPos3");
+
+            }
+            if (camDropdown.value == 3)
+            {
+                Debug.Log("CamPos4");
+
+            }
+        }
+    }
+}
