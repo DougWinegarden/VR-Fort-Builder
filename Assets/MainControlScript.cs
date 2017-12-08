c144f851ea0344b41a0863a33fd569610d8048eb
Jayse Farrell <jafarrellpersonal@gmail.com>
2017-12-07 21:02:25 -0800

    various changes

---
+++ Assets/MainControlScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControlScript : MonoBehaviour {
+	
+	public SteamVR_TrackedObject LeftController;
+	public SteamVR_TrackedObject RightController;
+
+	public SteamVR_Controller.Device LeftControllerDevice
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)LeftController.index);
+        }
+    }
+
+    public SteamVR_Controller.Device RightControllerDevice
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)RightController.index);
+        }
+    }
+
+    public GameObject laser;
+
+	// Use this for initialization
+	void Start () {
+        //SteamVR_Render.instance.pauseGameWhenDashboardIsVisible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (RightControllerDevice.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(RightController.transform.position, RightController.transform.forward, out hit, 100))
+            {
+
+            }
+        }
+	}
+
+    private void ShowLaser(RaycastHit hit, SteamVR_TrackedObject controller)
+    {
+        laser.SetActive(true);
+
+        laser.transform.position = Vector3.Lerp(controller.transform.position, hit.point, .5f);
+
+        laser.transform.LookAt(hit.point);
+
+
+        var temp = laser.transform.forward;
+        laser.transform.forward = laser.transform.up;
+        laser.transform.up = temp;
+
+        laser.transform.localScale = new Vector3(laser.transform.localScale.x, (controller.transform.position - hit.point).magnitude, laser.transform.localScale.z);
+    }
+}
---
+++ Assets/ViveController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveController : MonoBehaviour
+{
+
+    private SteamVR_TrackedObject controller;
+    public GameObject VR_Rig;
+    public GameObject laser;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)controller.index); }
+    }
+
+    void Awake()
+    {
+        controller = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Grip))
+        {
+            //Debug.Log("Right Controller Grip Pressed");
+            VR_Rig.transform.Rotate(new Vector3(0, 70 * Time.deltaTime, 0));
+        }
+
+        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //Debug.Log("Right Controller Trigger Pressed");
+            //controller.transform.
+        }
+
+
+
+        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(controller.transform.position, transform.forward, out hit, 100))
+            {
+                ShowLaser(hit);
+            }
+            else
+            {
+                laser.SetActive(false);
+                laser.GetComponent<Renderer>().enabled = false;
+            }
+        }
+        else if (!Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad)) // 3
+        {
+            laser.SetActive(false);
+            laser.GetComponent<Renderer>().enabled = false;
+            if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(controller.transform.position, transform.forward, out hit, 100))
+                {
+                    var newPosition = VR_Rig.transform.position;
+
+                    newPosition.x = hit.point.x;
+                    newPosition.z = hit.point.z;
+
+                    VR_Rig.transform.position = newPosition;
+                }
+
+            }
+        }
+    }
+
+    private void ShowLaser(RaycastHit hit)
+    {
+        Transform lzrTForm = laser.transform;
+
+        laser.SetActive(true);
+        laser.GetComponent<Renderer>().enabled = true;
+
+        lzrTForm.position = Vector3.Lerp(controller.transform.position, hit.point, .5f);
+
+        laser.transform.LookAt(hit.point);
+        laser.transform.forward = laser.transform.up;
+        
+        lzrTForm.localScale = new Vector3(lzrTForm.localScale.x, hit.distance / 2, lzrTForm.localScale.z);
+    }
+}
