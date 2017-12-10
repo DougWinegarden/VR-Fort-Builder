65ddf77ea618511a566315a168eaa4fd3cf32914
Doug Winegarden <d3wine@gmail.com>
2017-12-09 22:56:20 -0800

    Added select laser

--- Assets/ViveController.cs
+++ Assets/ViveController.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿// This is the script for the right controller.
+// the script for the left controller is called
+// LeftViveController
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +12,10 @@ public class ViveController : MonoBehaviour
 
     private SteamVR_TrackedObject controller;
     public GameObject VR_Rig;
-    public GameObject laser;
+    public GameObject teleLaser;
+
+    public GameObject selectLaser;
+
     private SteamVR_Controller.Device Controller
     {
         get { return SteamVR_Controller.Input((int)controller.index); }
@@ -27,17 +35,44 @@ public class ViveController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // rotate the player to the right. Makes me a bit dizzy if 
+        // I hold it down too long. I recommend just turning around in
+        // the room if you can lol
         if (Controller.GetPress(SteamVR_Controller.ButtonMask.Grip))
         {
             //Debug.Log("Right Controller Grip Pressed");
             VR_Rig.transform.Rotate(new Vector3(0, 70 * Time.deltaTime, 0));
         }
 
+
+
         if (Controller.GetPress(SteamVR_Controller.ButtonMask.Trigger))
         {
-            //Debug.Log("Right Controller Trigger Pressed");
-            //controller.transform.
+            RaycastHit hit;
+            if (Physics.Raycast(controller.transform.position, transform.forward, out hit, 100))
+            {
+                ShowSelectLaser(hit);
+                selectLaser.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+
+                if (hit.transform.GetComponent<fortBuilderObj>().selectable)
+                {
+                    selectLaser.GetComponent<Renderer>().material.color = new Color(0, 0, 255);
+                }
+            }
+            else
+            {
+                //selectLaser.transform.up = transform.forward;
+                //selectLaser.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+
+                //selectLaser.SetActive(false);
+                //selectLaser.GetComponent<Renderer>().enabled = false;
+            }
+            
+        }
+        else if (!Controller.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            selectLaser.SetActive(false);
+            selectLaser.GetComponent<Renderer>().enabled = false;
         }
 
 
@@ -48,48 +83,66 @@ public class ViveController : MonoBehaviour
 
             if (Physics.Raycast(controller.transform.position, transform.forward, out hit, 100))
             {
-                ShowLaser(hit);
+                ShowTeleLaser(hit);
             }
             else
             {
-                laser.SetActive(false);
-                laser.GetComponent<Renderer>().enabled = false;
+                teleLaser.SetActive(false);
+                teleLaser.GetComponent<Renderer>().enabled = false;
             }
         }
         else if (!Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad)) // 3
         {
-            laser.SetActive(false);
-            laser.GetComponent<Renderer>().enabled = false;
+            teleLaser.SetActive(false);
+            teleLaser.GetComponent<Renderer>().enabled = false;
             if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
             {
                 RaycastHit hit;
 
                 if (Physics.Raycast(controller.transform.position, transform.forward, out hit, 100))
                 {
-                    var newPosition = VR_Rig.transform.position;
+                    if (hit.transform.GetComponent<fortBuilderObj>())
+                    {
+                        var newPosition = VR_Rig.transform.position;
 
-                    newPosition.x = hit.point.x;
-                    newPosition.z = hit.point.z;
+                        newPosition.x = hit.point.x;
+                        newPosition.z = hit.point.z;
 
-                    VR_Rig.transform.position = newPosition;
+                        VR_Rig.transform.position = newPosition;
+                    }
                 }
 
             }
         }
     }
 
-    private void ShowLaser(RaycastHit hit)
+    private void ShowTeleLaser(RaycastHit hit)
     {
-        Transform lzrTForm = laser.transform;
+        Transform lzrTForm = teleLaser.transform;
 
-        laser.SetActive(true);
-        laser.GetComponent<Renderer>().enabled = true;
+        teleLaser.SetActive(true);
+        teleLaser.GetComponent<Renderer>().enabled = true;
 
         lzrTForm.position = Vector3.Lerp(controller.transform.position, hit.point, .5f);
 
-        laser.transform.LookAt(hit.point);
-        laser.transform.forward = laser.transform.up;
+        teleLaser.transform.LookAt(hit.point);
+        teleLaser.transform.forward = teleLaser.transform.up;
         
+        lzrTForm.localScale = new Vector3(lzrTForm.localScale.x, hit.distance / 2, lzrTForm.localScale.z);
+    }
+
+    private void ShowSelectLaser(RaycastHit hit)
+    {
+        Transform lzrTForm = selectLaser.transform;
+
+        selectLaser.SetActive(true);
+        selectLaser.GetComponent<Renderer>().enabled = true;
+
+        lzrTForm.position = Vector3.Lerp(controller.transform.position, hit.point, .5f);
+
+        selectLaser.transform.LookAt(hit.point);
+        selectLaser.transform.forward = selectLaser.transform.up;
+
         lzrTForm.localScale = new Vector3(lzrTForm.localScale.x, hit.distance / 2, lzrTForm.localScale.z);
     }
 }
