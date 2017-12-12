3242b0e5ae128b6c5078bf40126fcaac927d29da
Andrew Watson <watsonap6@gmail.com>
2017-12-11 21:20:57 -0800

    Fix for missing prefab

---
+++ Assets/ASL/Resources/FortBuilder/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private float timeToLive = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeToLive -= Time.deltaTime;
+        if (timeToLive <= 0) Destroy(gameObject);
+	}
+}
